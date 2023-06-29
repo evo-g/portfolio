f9d834f2ed046a2ac9d452a1e1f1f2d21916da1c
Evodio Garcia <evodiog@gmail.com>
2023-06-29 10:14:01 -0700

    update styles to default page

--- src/js/routes/DefaultPage/DefaultPage.jsx
+++ src/js/routes/DefaultPage/DefaultPage.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import cannot from '../../../assets/cannot-find.jpg'
 
 const DefaultPage = () => {
   return (
-    <section class="wrapper" id="default">
+    <section className="page-container" id="default">
       <h1>Sorry cannot find this page</h1>
-      <img src={require('../../../images/cannot-find.jpg')} alt='confused funny face' />
-      <Link to='/'>Return Home</Link>
+      <img src={cannot} alt='confused funny face' />
+      <Link className='btn' to='/'>Return Home</Link>
     </section>
   )
 };
